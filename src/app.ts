0b339b5b514cfff2a0b21482e60d2c81f4b8c8e0
jasbel <asbel.elmec@gmail.com>
2021-05-19 21:33:35 -0400

    step x: Swagger

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,11 @@ import morgan from 'morgan';
 
 //Swagger
 import swaggerUI from 'swagger-ui-express'
-import swaggerJsDock from 'swagger-jsdoc'
-
-
-import taskRoutes from './routes/tasks.routes'
+import swaggerJsDoc from 'swagger-jsdoc'
 import { options } from './swaggerOptions';
+
+
+import tasksRoutes from './routes/tasks.routes'
 
 const app = express()
 
@@ -18,9 +18,9 @@ app.use(cors()) /* comunicacion con otros servidores */
 app.use(morgan('dev')) /* Ver las peticiones */
 app.use(express.json()) /* Conversion a JSON */
 
-const specs = swaggerJsDock(options)
+const specs = swaggerJsDoc(options)
 
-app.use(taskRoutes);
+app.use(tasksRoutes);
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(specs));
 
 export default app
--- src/routes/tasks.routes.ts
+++ src/routes/tasks.routes.ts
@@ -1,11 +1,121 @@
 import {Router} from 'express';
 import { createTask, deleteTask, getTask, getTasks, updateTask, countTasks } from '../controllers/tasks.controller';
 
-const router = Router()
+const router = Router();
+
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    Task:
+ *      type: object
+ *      properties:
+ *        id:
+ *          type: string
+ *          description: the auto-generate id of task
+ *        description:
+ *          type: string
+ *          description: the description of the task
+ *      required:
+ *        - name
+ *        - description
+ *      example:
+ *        id: fsaodofsoidk8sd9fssadf7s8a
+ *        name: My First Task
+ *        description: I have to do something
+ *  parameters:
+ *    taskId:
+ *      in: path
+ *      name: id
+ *      required: true
+ *      
+ */
+
+/**
+ * @swagger
+ * tags:
+ *  name: Tasks
+ *  description: Tasks endpoint
+ */
+
+/**
+ * @swagger
+ * /task:
+ *  get:
+ *    summary: Return a Task List
+ *    tags: [Tasks]
+ *    responses:
+ *      200:
+ *        description: the list of tasks
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/Task'
+ */
 
 router.get('/tasks', getTasks)
+
+/**
+ * @swagger
+ * /tasks/count:
+ *  get:
+ *    summary: Get Total task Count
+ *    tags: [Tasks]
+ *    responses:
+ *      200:
+ *        description: the total number of tasks
+ *        content:
+ *          text/plain:
+ *            schema:
+ *              type: integer
+ *              example: 5
+ */
 router.get('/tasks/count', countTasks)
+
+/**
+ * @swagger
+ * /tasks:
+ *  post:
+ *    summary: create a new task
+ *    tags: [Tasks]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Task'
+ *    responses:
+ *      200:
+ *        description: The task successfully created (Tarea fue creada con exito )
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Task'
+ *      500:
+ *        description: some server error (Algun error del servidor)
+ */
 router.post('/tasks', createTask)
+
+/**
+ * @swagger
+ * /tasks/{id}:
+ *  post:
+ *    summary: Get a task by id
+ *    tags: [Tasks]
+ *    parameters:
+ *     
+ *    responses:
+ *      200:
+ *        description: The task successfully created (Tarea fue creada con exito )
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Task'
+ *      500:
+ *        description: some server error (Algun error del servidor)
+ */
 router.get('/tasks/:id', getTask)
 router.delete('/tasks/:id', deleteTask)
 router.put('/tasks/:id', updateTask)
